5ad35e83dc213fb8b11db053b5fc5152c0167c3f
Konstantin Holzhausen <Konstantin-Holzhausen@t-online.de>
2022-09-09 10:57:40 +0200

    commiting changes

--- LocalLearning/Statistics.py
+++ LocalLearning/Statistics.py
@@ -1,5 +1,6 @@
 import torch
 from torch import Tensor
+from torch.nn import Module
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 import numpy as np
@@ -7,46 +8,51 @@ import numpy as np
 # custom modules
 from .LocalLearning import KHL3
 
+
 def stringer_spectrum(nu):
     return 1 / nu
 
+
 def cov_spectrum(
-        dataloader: DataLoader, model: KHL3, device: torch.device
+        dataloader: DataLoader, model: Module, device: torch.device, dtype: torch.dtype=torch.float32
 ) -> Tensor:
     """
-    Calculate the ordered spectrum of the covariance matrix
+    Calculate the ordered singular value spectrum of the covariance matrix
     """
     with torch.no_grad():
+
+        noE = model.pSet["hidden_size"]
+
+        """
+        Calculate covariance matrix on shifted data to increase
+        computational stability
+        """
+
+        # initialize stat tensors
+        auto_corr = torch.zeros((noE, noE), device=device, dtype=dtype)
+        mean = torch.zeros((noE,), device=device, dtype=dtype)
+        pop_size = 0
+
+        # shift = estimate of the mean
+        f, _ = next(iter(dataloader))
+        K = f.mean()
+
         with tqdm(dataloader, unit="batch") as ttest:
-
-            noE = model.pSet["hidden_size"]
-
-            """
-            Calculate covariance matrix on shifted data to increase
-            computational stability
-            """
-
-            # initialize stat tensors
-            corr = torch.zeros((noE, noE), device=device)
-            mean = torch.zeros((noE,), device=device)
-            pop_size = 0
-
             ttest.set_description(f"Calculating covariance spectrum")
-
-            # shift = estimate of the mean
-            f, label = next(iter(dataloader))
-            K = f.mean()
-
-            for x, label in ttest:
+            for x, _ in ttest:
                 pop_size += len(x)
                 pred = model(x.to(device))
-                corr += (pred.T - K) @ (pred - K)
-                mean += pred.sum(axis=0) - K
+                data = pred - K
+                auto_corr += data.T @ data
+                mean += data.sum(axis=0)
 
-            cov = corr - mean.T @ mean / pop_size
-            cov /= pop_size - 1
+        cov = auto_corr - mean[None].T @ mean[None] / pop_size
+        cov /= pop_size - 1
 
-            l_real = torch.linalg.eigvals(cov).real
-            l_sorted, idx = torch.sort(l_real, descending=True)
-            return l_sorted
+        # cov matrix is real valued
+        # -> [cov, cov.adjoint] = 0
+        # -> eigen values are real
+        l_real = torch.linalg.eigvals(cov).real
+        l_n, _ = torch.sort(l_real, descending=True)
 
+        return l_n
