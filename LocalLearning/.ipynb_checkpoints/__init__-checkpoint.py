67d78fcc8da2524c779bb15fb75525560a02c189
Konstantin Holzhausen <Konstantin-Holzhausen@t-online.de>
2022-09-09 13:26:25 +0200

    clean up

--- LocalLearning/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,4 +0,0 @@
-from .LocalLearning import LocalLearningModel
-from .LocalLearning import train_unsupervised
-from .Statistics import cov_spectrum
-from .Statistics import stringer_spectrum
--- LocalLearning/__init__.py
+++
@@ -1,7 +0,0 @@
-from .LocalLearning import KHL3
-from .LocalLearning import FKHL3
-from .LocalLearning import train_unsupervised
-from .LocalLearning import BioLearningModel
-from .LocalLearning import LpUnitCIFAR10
-from .Statistics import cov_spectrum
-from .Statistics import stringer_spectrum
